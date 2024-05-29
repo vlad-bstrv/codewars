3486dda1f3f01711a2bc311b35c63ab87d0a5ec6
vladbstrv <keocer@gmail.com>
2024-05-29 12:08:02 +0300

    MultiplicationTable.kt

---
+++ kyu6/MultiplicationTable.kt
@@ -0,0 +1,19 @@
+package kyu6
+
+/**
+ * Your task, is to create N×N multiplication table, of size provided in parameter.
+ *
+ * For example, when given size is 3:
+ * 1 2 3
+ * 2 4 6
+ * 3 6 9
+ *
+ * For the given example, the return value should be:
+ * [[1,2,3],[2,4,6],[3,6,9]]
+ */
+
+object KataSolution {
+    fun multiplicationTable(size: Int) = Array(size) { i ->
+        (1..size).map { it * (i + 1) }.toIntArray()
+    }
+}
